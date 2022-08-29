3a2504b6229026491b64eb19bb3fa57cd68d44f3
Dmitrii <shokilla@mail.ru>
2022-08-29 12:05:48 +0400

    created first slyled

--- src/components/styledComponents/StyledComp.jsx
+++ src/components/styledComponents/StyledComp.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledWrapper = styled.div`
+  width: 100%;
+  min-height: 95vh;
+  padding: 2rem;
+  background: black;
+`;
 
 const StyledComp = () => {
-  return <div>StyledComp</div>;
+  return <StyledWrapper>StyledComp</StyledWrapper>;
 };
 
 export default StyledComp;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
+import styled, { createGlobalStyle } from "styled-components";
 import App from "./App";
+
+const Global = createGlobalStyle`
+*{
+  margin: 5px;
+  padding: 10px;
+  box-sizing: border-box
+}
+`;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
